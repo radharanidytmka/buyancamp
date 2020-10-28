dfb4d57b4861c5e55cb881d7b0afe97d2a0a6911
radharanidytmka <radha.dyatmika@gmail.com>
2020-10-28 14:37:11 +0800

    refer login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -31,7 +31,8 @@
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#team">Team</a></li>
-                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="/login">Booking</a></li>
                     </ul>
                 </div>
             </div>
@@ -39,7 +40,7 @@
         <!-- Masthead-->
         <header class="masthead">
             <div class="container">
-                <div class="masthead-subheading">Welcome To Our Studio!</div>
+                <div class="masthead-subheading">Welcome To Tepi Buyan Campfire!</div>
                 <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
                 <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
             </div>
